f01d59d3db8efd2a0e7c17b7b516a3bb55714e93
divyanshugarg2017 <divyanshugarg1995@gmail.com>
2022-08-25 13:53:45 +0530

    added carousel

--- src/pages/nftItem/index.js
+++ src/pages/nftItem/index.js
@@ -6,6 +6,9 @@ import TopSection from './components/topsection/topSection'
 import Overview from './components/overview/overview'
 import Bids from './components/bids/bids'
 import NftItemCard from '../../components/Card/nftItemCard/nftItemCard'
+import { collectioncard_data } from '../../static/collectionSpotlight'
+import Carousel from "react-multi-carousel";
+
 
 const sections = [
     { name: "Overview", id: 1 },
@@ -13,6 +16,26 @@ const sections = [
     { name: "Bids", id: 3 },
     { name: "Activity", id: 4 },
 ]
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
 
 function NftItem() {
     const [selected, setSelected] = useState("Overview")
@@ -22,77 +45,38 @@ function NftItem() {
             <div className='mt-14 pb-6 flex flex-row'>
                 <div className="basis-1/2 ">
                     <div className='px-14'>
-                    <figure className='flex justify-center pb-6'>
-                        <img className='w-full' src={image1} alt="pic" />
-                    </figure>
-                    <div className='pt-6 flex justify-center pb-14'>
-                        <div className='w-full h-[80px] bg-[#303f50] backdrop-blur-2xl opacity-90 rounded-2xl p-4 py-3 flex justify-around' >
-                            {sections.map((item) => {
-                                return (<div onClick={() => { setSelected(item.name) }} className={selected == item.name ? "text-foreground-primary border border-[#9b71d8]  rounded-xl cursor-pointer bg-[#9b71d8] text-base font-bold px-[14px] py-[16px]" : "text-foreground-primary cursor-pointer text-base font-bold px-[14px] py-[16px]"}><div>
-                                    {item.name}
-                                </div>
-                                </div>)
-                            })}
+                        <figure className='flex justify-center pb-6'>
+                            <img className='w-full' src={image1} alt="pic" />
+                        </figure>
+                        <div className='pt-6 flex justify-center pb-14'>
+                            <div className='w-full h-[80px] bg-[#303f50] backdrop-blur-2xl opacity-90 rounded-2xl p-4 py-3 flex justify-around' >
+                                {sections.map((item) => {
+                                    return (<div onClick={() => { setSelected(item.name) }} className={selected == item.name ? "text-foreground-primary border border-[#9b71d8]  rounded-xl cursor-pointer bg-[#9b71d8] text-base font-bold px-[14px] py-[16px]" : "text-foreground-primary cursor-pointer text-base font-bold px-[14px] py-[16px]"}><div>
+                                        {item.name}
+                                    </div>
+                                    </div>)
+                                })}
+                            </div>
                         </div>
-                    </div>
-                    <div className='pb-10'>
-                        {selected==="Overview" && <Overview/>}
-                        {selected==="Bids" && <Bids/>}
-                    </div>
-                    <div className='text-foreground-primary text-shadow text-xl font-semibold pb-10'>More From This Collection</div>
-                    <div><NftItemCard/></div>
+                        <div className='pb-10'>
+                            {selected === "Overview" && <Overview />}
+                            {selected === "Bids" && <Bids />}
+                        </div>
+                        <div className='text-foreground-primary text-shadow text-xl font-semibold pb-10'>More From This Collection</div>
                     </div>
                 </div>
-                <div className='px-14 basis-1/2'>
-                    <div className='text-sm font-bold text-foreground-primary py-6'>Modernist Masks</div>
-                    <div className='text-3xl font-bold text-foreground-primary pb-10'>Cratey's #108</div>
-                    <div className='columns-2 pb-6'>
-                        <div className='flex border-r'>
-                            <div className='w-12 h-12 rounded-full mr-6 bg-gradient-to-r from-[#FD3DCE] to-[#00F0FF]'></div>
-                            <div>
-                                <div className='text-foreground-primary text-xs font-normal'> Creator</div>
-                                <div className='text-foreground-primary text-base font-semibold'>Pransky</div>
-                            </div>
-                        </div>
-                        <div className='flex pl-6'>
-                            <div className='w-12 h-12 rounded-full mr-6 bg-gradient-to-r from-[#FD3DCE] to-[#00F0FF]'></div>
-                            <div>
-                                <div className='text-foreground-primary text-xs font-normal'> Current Owner</div>
-                                <div className='text-foreground-primary text-base font-semibold'>Cradle</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pb-6'>
-                        <div className='flex'>
-                            <div className='flex text-[#BFCBD9]'><FaRegHeart className='text-xl' /><div className='pl-4'>225</div></div>
-                            <div className='flex text-[#BFCBD9] pl-6'><MdIosShare className='text-xl' /><div className='pl-4'>225</div></div>
-                        </div>
-                    </div>
-                    <div className='bg-[#121a23] backdrop-blur-xl w-full h-32 rounded-lg border mb-6'>
-                        <div className='columns-2 p-6'>
-                            <div>
-                                <div className='text-foreground-primary opacity-60 text-xs font-semibold pb-2'>Price</div>
-                                <div className='text-foreground-primary text-xl font-semibold pb-2'>81.8 ETH</div>
-                                <div className='text-foreground-primary opacity-60 text-xs font-semibold'>$143868</div>
-                            </div>
-                            <div>
-                                <div>
-                                    <div className='text-foreground-primary opacity-60 text-xs font-semibold pb-2'>Price</div>
-                                    <div className='background-linear-gradient text-foreground-primary text-xl font-semibold pb-2'>0.1 w-ETH</div>
-                                    <div className='text-foreground-primary opacity-60 text-xs font-semibold'>by <span>Penguin</span></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='text-xs font-semibold text-foreground-primary opacity-60 pb-6'>
-                        Last sale price <span className='text-base text-foreground-primary font-semibold pl-2'>81.8 ETH</span>
-                    </div>
-                    <div className='flex'>
-                        <div className='btn btn-background'>Buy Now for 81.8 Eth</div>
-                        <div className='btn secondary-btn-background w-48 ml-4'>Place a Bid</div>
-                    </div>
+                <div>
+                    <TopSection />
                 </div>
+                
             </div>
+            <div className="grid grid-cols-1  items-center   ">
+                                <Carousel responsive={responsive}>
+                                    {collectioncard_data.map((resource, index) => {
+                                        return <NftItemCard />;
+                                    })}
+                                </Carousel>
+                            </div>
         </div>
     )
 }
