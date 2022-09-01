62f0f398d2129778968ddf7360dc1d228b991dba
divyanshugarg2017 <divyanshugarg1995@gmail.com>
2022-09-01 16:07:42 +0530

    minor css fix

--- src/pages/nftItem/components/activity/activityTable/activityTable.js
+++ src/pages/nftItem/components/activity/activityTable/activityTable.js
@@ -5,9 +5,9 @@ function ActivityTable() {
     <div class="overflow-x-auto">
             <table class="table w-full">
                 <tbody>
-                    <tr>
+                    <tr className='border-none'>
                         {/* <th></th> */}
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='text-base font-semibold text-foreground-primary'><span className='text-[#23AEE3] pr-2'>Pransky</span><span>listed for</span></div>
                             <div className='flex'>
                                 <div className='text-[#BFCBD9] text-sm pr-4'>8/8/22</div>
@@ -15,16 +15,16 @@ function ActivityTable() {
                                 
                             </div>
                         </td>
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='float-right'>
                                 <div className='text-base font-semibold text-foreground-primary'>0.1 wETH</div>
                                 <div className='text-[#BFCBD9] text-sm'>$171</div>
                             </div>
                         </td>
                     </tr>
-                    <tr>
+                    <tr className='border-none'>
                         {/* <th></th> */}
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='text-base font-semibold text-foreground-primary'><span className='text-[#23AEE3] pr-2'>Pransky</span><span>listed for</span></div>
                             <div className='flex'>
                                 <div className='text-[#BFCBD9] text-sm pr-4'>8/8/22</div>
@@ -32,16 +32,16 @@ function ActivityTable() {
                                 
                             </div>
                         </td>
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='float-right'>
                                 <div className='text-base font-semibold text-foreground-primary'>0.1 wETH</div>
                                 <div className='text-[#BFCBD9] text-sm'>$171</div>
                             </div>
                         </td>
                     </tr>
-                    <tr>
+                    <tr className='border-none'>
                         {/* <th></th> */}
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='text-base font-semibold text-foreground-primary'><span className='text-[#23AEE3] pr-2'>Pransky</span><span>listed for</span></div>
                             <div className='flex'>
                                 <div className='text-[#BFCBD9] text-sm pr-4'>8/8/22</div>
@@ -49,16 +49,16 @@ function ActivityTable() {
                                 
                             </div>
                         </td>
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='float-right'>
                                 <div className='text-base font-semibold text-foreground-primary'>0.1 wETH</div>
                                 <div className='text-[#BFCBD9] text-sm'>$171</div>
                             </div>
                         </td>
                     </tr>
-                    <tr>
+                    <tr className='border-none'>
                         {/* <th></th> */}
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='text-base font-semibold text-foreground-primary'><span className='text-[#23AEE3] pr-2'>Pransky</span><span>listed for</span></div>
                             <div className='flex'>
                                 <div className='text-[#BFCBD9] text-sm pr-4'>8/8/22</div>
@@ -66,7 +66,7 @@ function ActivityTable() {
                                 
                             </div>
                         </td>
-                        <td className='bg-[#121a23]'>
+                        <td className='bg-[#121a23] backdrop-blur-xl bg-opacity-30 border-none'>
                             <div className='float-right'>
                                 <div className='text-base font-semibold text-foreground-primary'>0.1 wETH</div>
                                 <div className='text-[#BFCBD9] text-sm'>$171</div>
