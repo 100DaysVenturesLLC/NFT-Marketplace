ef3575839bd001827f96db984b365d3ed7db1432
Akhil Bharti <akhil.bh96@gmail.com>
2022-09-16 05:17:07 +0530

    added slider component and NFT ABI

---
+++ src/pages/stfu/components/Slider.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 400,
+  },
+});
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+export default function CountSlider({handleUpdate}) {
+  const classes = useStyles();
+  const handleChange = (e,value) =>{
+    console.log("slider ka target",value)
+    handleUpdate(value)
+  }
+  return (
+    <div style={{textAlign:'center'}} className={classes.root}>
+      <Slider
+        style={{marginRight:'50px',width:'80%',color:'#FD3DCE'}}
+        color="primary"
+        defaultValue={1}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={1}
+        max={10}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
